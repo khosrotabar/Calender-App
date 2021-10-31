6596167577ae1aa40c9fe4dc49d8fc7dc11ed629
Mohammad Khosrotabar <mh.khosrotabar75@gmail.com>
2021-10-31 10:13:04 +0330

    Calendar App

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Weeks } from "./DateData";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.css";
 import {
   JanuaryDays,
   FebruaryDays,
